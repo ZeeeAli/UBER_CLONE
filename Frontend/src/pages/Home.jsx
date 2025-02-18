9b7fa506b06d17319544673d08cceeb9a1fff4ca
Zeeshan Ali <za4116793@gmail.com>
2025-02-18 20:29:10 +0500

    changes

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
           alt=""
         />
         <div className="bg-white pb-7 px-4 py-4">
-          <h2 className="text-[40px] font-bold">Get Started With Uber</h2>
+          <h2 className="text-[30px] font-bold">Get Started With Uber</h2>
           <Link
             to="/login"
             className="flex items-center justify-center w-full bg-black text-white py-3 rounded mt-5"
